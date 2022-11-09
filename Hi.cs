9832e955f45c0b9c6dbc03557c42c53e3c69bdee
Milosz <milosz.moroz@pw.edu.pl>
2022-11-09 22:59:16 +0100

    hi.cs edited

--- Hi.cs
+++ Hi.cs
@@ -5,7 +5,8 @@
         public static void Hi()
         {
             // To show a message box when this method from `WelcomeClass` will be called by another class
-            Console.WriteLine("Hi, World");
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("DDD");
         }
     }
 }
