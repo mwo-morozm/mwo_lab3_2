33ee10184b77ebf715388315ee7ce9563e6ffa8e
Milosz <milosz.moroz@pw.edu.pl>
2022-11-09 22:28:19 +0100

    added Hi.cs, changed Program.cs

---
+++ Hi.cs
@@ -0,0 +1,11 @@
+﻿namespace DDD
+{
+    public class HiClass
+    {
+        public static void Hi()
+        {
+            // To show a message box when this method from `WelcomeClass` will be called by another class
+            Console.WriteLine("Hi, World");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static DDD.HiClass;
+
+namespace IncludeClass
+{
+    public partial class Form1
+    {
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hi();
+        }
+    }
+}
